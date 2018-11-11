c741c737091b66f07cbd81d56d99a13d5faf082f
Liam Dawson <liam@ldaws.com>
2018-11-12 01:08:30 +1100

    start moving to Tachyons

--- src/About/index.tsx
+++ src/About/index.tsx
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import portrait from "./portrait.png";
-import "./About.scss";
 
 class App extends Component {
   render(): React.ReactNode {
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
-import "./App.scss";
 import AboutPage from "./About";
 import PrintingPage from "./Printing";
 import NotFoundPage from "./NotFound";
@@ -8,7 +7,7 @@ import NotFoundPage from "./NotFound";
 class App extends Component {
   render(): React.ReactNode {
     return (
-      <>
+      <div className="sans-serif f4 lh-copy">
         <BrowserRouter>
           <Switch>
             <Route exact path="/" component={AboutPage} />
@@ -17,14 +16,14 @@ class App extends Component {
             <Route component={NotFoundPage} />
           </Switch>
         </BrowserRouter>
-        <footer className="pageFooter" role="banner">
+        <footer className="pt4 pb4 tc f5" role="banner">
           <p>
             This page brought to you by many electrons, photons and other tiny
             particles.
           </p>
           <p>Any mistakes were probably my cat sitting on the keyboard.</p>
         </footer>
-      </>
+      </div>
     );
   }
 }
